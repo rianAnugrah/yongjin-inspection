9184a923ab209f8910d96477e5db1054d41891c5
Rian Anugrah <rian.te.quiero@gmail.com>
2024-10-07 10:54:46 +0700

    Fix Runtime Error

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
 
-fastify.register(require('fastify-static'), {
+fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'public')
 })
-fastify.register(require('fastify-formbody'))
+fastify.register(require('@fastify/formbody'))
 fastify.register(require('point-of-view'), {
   engine: {
     ejs: require('ejs'),
