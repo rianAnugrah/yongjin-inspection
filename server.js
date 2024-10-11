51b48255c7d47202e9cb78185d3780af999c509f
Rian Anugrah <rian.te.quiero@gmail.com>
2024-10-11 23:29:38 +0700

    asdasda

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ fastify.register(require('./db'))
 
 fastify.register(require('./routes/index'))
 
-fastify.listen(3000, (err) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
